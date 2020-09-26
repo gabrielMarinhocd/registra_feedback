89b2d966747bb04cfc105300d651b858a49a4688
Gabriel_Marinho <biel32771@gmail.com>
2020-09-26 19:29:20 -0300

    Ajustes na api

--- index.js
+++ index.js
@@ -26,20 +26,9 @@ app.use(express.json());
 
 app.use(express.json());
 
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
-app.get('/', function (req, res, next) {
+app.get('/', function (req, res) {
   res.send('Ok api iniciada ');
   console.log('GET OK');
-
-  next();
 });
 
 app.use('/', routeInsertFeedback);
