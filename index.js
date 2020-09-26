3a35016bed4eecc402618a24ee7edb8a10c0202a
Gabriel_Marinho <biel32771@gmail.com>
2020-09-26 19:39:39 -0300

    Ajustes na api

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(express.json());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', '191.176.91.228'); // update to match the domain you will make the request from
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
